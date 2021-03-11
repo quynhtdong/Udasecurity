c2c51864825a5a65ef9147f0325fe6be12eb140a
Quynh <quynhdongikn@gmail.com>
2021-03-12 00:13:29 +0700

    separate module

---
+++ Security/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.catpoint.security {
+    requires miglayout;
+    requires java.desktop;
+    requires guava;
+    requires gson;
+    requires java.prefs;
+    requires com.udacity.catpoint.image;
+}
